4ddb49d2d09adb612a99832fb794ac4577aa3b43
Paul Keller <contact.keller.paul@gmail.com>
2022-12-02 13:10:31 +0100

    day 2 and templating

--- tempfolder/puzzle.py
+++ tempfolder/puzzle.py
@@ -2,10 +2,11 @@
 import os
 import sys
 
+INPUT_FILE="inputtest"
 
 def main():
   """Main Function called on Startup"""
-  input_text= open(os.path.join(sys.path[0], 'input'),'r', encoding="UTF-8")
+  input_text= open(os.path.join(sys.path[0], INPUT_FILE),'r', encoding="UTF-8")
   lines = input_text.readlines()
 
 if __name__ == "__main__":
