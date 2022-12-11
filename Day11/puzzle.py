ff956677f26b5206437e6152c5a3cef3863c9ccd
Paul Keller <contact.keller.paul@gmail.com>
2022-12-11 10:14:13 +0100

    day 10

---
+++ Day11/puzzle.py
@@ -0,0 +1,18 @@
+"""provides path joining functions annd module joining"""
+import os
+import sys
+import time
+
+INPUT_FILE="sample"
+
+def main():
+  """Main Function called on Startup"""
+  input_text= open(os.path.join(sys.path[0], INPUT_FILE),'r', encoding="UTF-8")
+  lines = input_text.readlines()
+
+if __name__ == "__main__":
+  st = time.time()
+  main()
+  et = time.time()
+  evaluationtime = (et-st)*1000
+  print(f"Execution time: {evaluationtime}ms");
